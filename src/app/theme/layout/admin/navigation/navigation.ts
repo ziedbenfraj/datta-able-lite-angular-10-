f72b2a99c234492e8e27ce23d9e6134d3a1d2d1a
ziedbenfrajj <ziedbenfrajj@gmail.com>
2020-10-09 15:22:55 +0200

    sidebar fixed

--- src/app/theme/layout/admin/navigation/navigation.ts
+++ src/app/theme/layout/admin/navigation/navigation.ts
@@ -28,171 +28,51 @@ export interface Navigation extends NavigationItem {
 const NavigationItems = [
   {
     id: 'navigation',
-    title: 'test1',
     type: 'group',
-    icon: 'icon-navigation',
     children: [
       {
         id: 'dashboard',
-        title: 'dash',
+        title: 'Executive Dashboard',
         type: 'item',
         url: '/dashboard/default',
-        icon: 'feather icon-home',
+        icon: 'fas fa-tachometer-alt',
         classes: 'nav-item',
-      }
-    ]
-  },
-  {
-    id: 'ui-element',
-    title: 'UI ELEMENT',
-    type: 'group',
-    icon: 'icon-ui',
-    children: [
-      {
-        id: 'basic',
-        title: 'Component',
-        type: 'collapse',
-        icon: 'feather icon-box',
-        children: [
-          {
-            id: 'button',
-            title: 'Button',
-            type: 'item',
-            url: '/basic/button'
-          },
-          {
-            id: 'badges',
-            title: 'Badges',
-            type: 'item',
-            url: '/basic/badges'
-          },
-          {
-            id: 'breadcrumb-pagination',
-            title: 'Breadcrumb & Pagination',
-            type: 'item',
-            url: '/basic/breadcrumb-paging'
-          },
-          {
-            id: 'collapse',
-            title: 'Collapse',
-            type: 'item',
-            url: '/basic/collapse'
-          },
-          {
-            id: 'tabs-pills',
-            title: 'Tabs & Pills',
-            type: 'item',
-            url: '/basic/tabs-pills'
-          },
-          {
-            id: 'typography',
-            title: 'Typography',
-            type: 'item',
-            url: '/basic/typography'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    id: 'forms',
-    title: 'Forms & Tables',
-    type: 'group',
-    icon: 'icon-group',
-    children: [
+      },
       {
         id: 'forms-element',
-        title: 'Form Elements',
+        title: 'Engagement Dashboard',
         type: 'item',
         url: '/forms/basic',
         classes: 'nav-item',
-        icon: 'feather icon-file-text'
+        icon: 'fas fa-chart-line'
       },
       {
         id: 'tables',
-        title: 'Tables',
+        title: 'Competencies Dashboard',
         type: 'item',
         url: '/tables/bootstrap',
         classes: 'nav-item',
-        icon: 'feather icon-server'
-      }
-    ]
-  },
-  {
-    id: 'chart-maps',
-    title: 'Chart & Maps',
-    type: 'group',
-    icon: 'icon-charts',
-    children: [
+        icon: 'fas fa-share-alt-square'
+      },
       {
         id: 'charts',
-        title: 'Charts',
+        title: 'Knowldge Base',
         type: 'item',
         url: '/charts/morris',
         classes: 'nav-item',
-        icon: 'feather icon-pie-chart'
-      }
-    ]
-  },
-  {
-    id: 'pages',
-    title: 'Pages',
-    type: 'group',
-    icon: 'icon-pages',
-    children: [
-      {
-        id: 'auth',
-        title: 'Authentication',
-        type: 'collapse',
-        icon: 'feather icon-lock',
-        children: [
-          {
-            id: 'signup',
-            title: 'Sign up',
-            type: 'item',
-            url: '/auth/signup',
-            target: true,
-            breadcrumbs: false
-          },
-          {
-            id: 'signin',
-            title: 'Sign in',
-            type: 'item',
-            url: '/auth/signin',
-            target: true,
-            breadcrumbs: false
-          }
-        ]
+        icon: 'fas fa-street-view'
       },
       {
         id: 'sample-page',
-        title: 'Sample Page',
+        title: 'Ability Analysis',
         type: 'item',
         url: '/sample-page',
         classes: 'nav-item',
-        icon: 'feather icon-sidebar'
-      },
-      {
-        id: 'disabled-menu',
-        title: 'Disabled Menu',
-        type: 'item',
-        url: 'javascript:',
-        classes: 'nav-item disabled',
-        icon: 'feather icon-power',
-        external: true
-      },
-      {
-        id: 'buy_now',
-        title: 'Buy Now',
-        type: 'item',
-        icon: 'feather icon-book',
-        classes: 'nav-item',
-        url: 'https://codedthemes.com/item/datta-able-angular/',
-        target: true,
-        external: true
+        icon: 'fas fa-chart-pie'
       }
     ]
-  }
+  },
+  
 ];
 
 @Injectable()
